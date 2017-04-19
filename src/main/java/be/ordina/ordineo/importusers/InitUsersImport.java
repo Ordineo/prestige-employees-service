c51faa2c508e192ca52e20197a31847cac32f143
Tyranwyn <sammi.fux@hotmail.com>
2017-04-19 13:33:49 +0200

    PRES-92: Initial import of github users added.
    "Temporary solution: duplicate code. In class doesn't work"

--- src/main/java/be/ordina/ordineo/importusers/InitUsersImport.java
+++ src/main/java/be/ordina/ordineo/importusers/InitUsersImport.java
@@ -1,35 +1,64 @@
 package be.ordina.ordineo.importusers;
 
-import be.ordina.ordineo.model.Employee;
+import be.ordina.ordineo.model.Role;
 import be.ordina.ordineo.repo.EmployeeRepository;
+import be.ordina.ordineo.model.Employee;
+import be.ordina.ordineo.repo.RoleRepository;
 import org.kohsuke.github.GHOrganization;
 import org.kohsuke.github.GHUser;
 import org.kohsuke.github.GitHub;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
 /**
- * Created by SaFu on 19/04/2017.
+ * Created by SaFu on 14/04/2017.
  */
 @Component
-public class ScheduledTasks {
+public class InitUsersImport implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    EmployeeRepository employeeRepository;
 
-    @Scheduled(cron = "0 0 0 * * *")
-    public void importGitHubUsers() throws IOException {
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Initial users import started...");
+        createAdmin();
+        initImportUsers();
+    }
+
+    private void createAdmin() {
+        // Admin config
+        Role adminrole = new Role("admin");
+        roleRepository.save(adminrole);
+        Collection<Role> roles = new ArrayList<>();
+        roles.add(adminrole);
+        Employee admin = new Employee("admin", bCryptPasswordEncoder.encode("admin"), "sammi.fux@ordina.be",
+                "Sammi", "Fux", "https://media.licdn.com/media/AAEAAQAAAAAAAAVgAAAAJDRjNGE2ZWVkLTM3NzEtNDNhZC1hMDdiLTI3NmU4MGU5Mzk0Zg.jpg",
+                "0469696969", "JWORKS", "M", roles);
+
+        employeeRepository.save(admin);
+    }
+
+    private void initImportUsers() throws IOException {
         GitHub gh = GitHub.connect();
         GHOrganization organization = gh.getOrganization("Ordineo");
 
@@ -74,5 +103,4 @@ public class ScheduledTasks {
         }
         return new String[] {firstname, lastname.substring(0, lastname.length()-1)};
     }
-
 }
