f3e809730b6f2115c468aac153e3fea3caf3e16b
Tyranwyn <sammi.fux@hotmail.com>
2017-04-27 16:44:06 +0200

    PRES-92: move JWT config to gateway

--- src/main/java/be/ordina/ordineo/EmployeeApplication.java
+++ src/main/java/be/ordina/ordineo/EmployeeApplication.java
@@ -1,12 +1,16 @@
 package be.ordina.ordineo;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.config.EnableEntityLinks;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableEntityLinks
@@ -18,5 +22,17 @@ public class EmployeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+	/**
+	 * Start internal H2 server so we can query the DB from IDE
+	 *
+	 * @return H2 Server instance
+	 * @throws SQLException
+	 */
+	// TODO remove when using external db
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server h2Server() throws SQLException {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9901");
 	}
 }
