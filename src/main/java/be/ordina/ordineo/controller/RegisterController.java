f9e0d3ccdfe11a7322236b054339db5799f6220b
Tyranwyn <sammi.fux@hotmail.com>
2017-05-19 11:30:00 +0200

    PRES-92: Bugfix regarding setRoles list

--- src/main/java/be/ordina/ordineo/controller/RegisterController.java
+++ src/main/java/be/ordina/ordineo/controller/RegisterController.java
@@ -4,13 +4,12 @@ import be.ordina.ordineo.model.Employee;
 import be.ordina.ordineo.model.Role;
 import be.ordina.ordineo.repository.EmployeeRepository;
 import be.ordina.ordineo.repository.RoleRepository;
+import be.ordina.ordineo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by SaFu on 19/04/2017.
@@ -30,10 +29,17 @@ public class RegisterController {
     @RequestMapping(path = "/register", method = RequestMethod.POST)
     public void enableAccount(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password) {
         Employee employee = employeeRepository.findByUsername(username);
+        System.out.println(employee);
         employee.setPassword(bCryptPasswordEncoder.encode(password));
         employee.setEnabled(1);
         Role userRole = roleRepository.findByTitle("admin");
-        employee.setRoles((List<Role>) new HashSet<Role>(Arrays.asList(userRole)));
+        System.out.println(userRole);
+        // Arrays.asList
+        // Collections.sign...
+        List<Role> roles = new ArrayList<>();
+        roles.add(userRole);
+        employee.setRoles(roles);
+        System.out.println(employee);
         employeeRepository.save(employee);
     }
 
