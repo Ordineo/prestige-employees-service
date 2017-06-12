f7fe4d8e2398f22f5dd823bfee5db12644d45ee2
Maarten Casteels <Maarten.Casteels@ordina.be>
2017-06-12 11:12:36 +0200

    Update the configuration classes

--- src/main/java/be/ordina/ordineo/config/WebMvcConfig.java
+++ src/main/java/be/ordina/ordineo/config/WebMvcConfig.java
@@ -2,23 +2,15 @@ package be.ordina.ordineo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * Created by SaFu on 18/04/2017.
- */
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        return bCryptPasswordEncoder;
+        return new BCryptPasswordEncoder();
     }
 
 }
